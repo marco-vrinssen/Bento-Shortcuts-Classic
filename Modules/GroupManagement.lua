7780ca7afa477c279d22288208406d03cf751be8
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-04-02 09:14:45 +0200

    Add AuctionWhisper module and enhance GroupManagement with role check and party marking commands

--- Modules/GroupManagement.lua
+++ Modules/GroupManagement.lua
@@ -7,6 +7,17 @@ end
 SLASH_READYCHECK1 = "/rc"
 SlashCmdList["READYCHECK"] = readyCheck
 
+
+-- ROLE CHECK
+
+local function roleCheck()
+    InitiateRolePoll()
+end
+
+SLASH_ROLECHECK1 = "/rc+"
+SlashCmdList["ROLECHECK"] = roleCheck
+
+
 -- QUIT GROUP
 
 local function quitParty() 
@@ -17,3 +28,28 @@ end
 
 SLASH_QUITPARTY1 = "/q"
 SlashCmdList["QUITPARTY"] = quitParty
+
+
+-- MARK PARTY
+
+local function markParty()
+    if not IsInGroup() then
+        print("You are not in a group.")
+        return
+    end
+
+    for partyIndex = 1, GetNumGroupMembers() do
+        local unitId = "party" .. partyIndex
+        if UnitExists(unitId) then
+            local assignedRole = UnitGroupRolesAssigned(unitId)
+            if assignedRole == "TANK" then
+                SetRaidTarget(unitId, 6)
+            elseif assignedRole == "HEALER" then
+                SetRaidTarget(unitId, 1)
+            end
+        end
+    end
+end
+
+SLASH_MARKPARTY1 = "/mp"
+SlashCmdList["MARKPARTY"] = markParty
